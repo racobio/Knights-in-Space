d69a5c951867ccf28e7b666121c154642c8e1150
Lucianzjw <75048853+Lucianzjw@users.noreply.github.com>
2021-05-03 17:05:48 +0800

    Add files via upload

---
+++ 4550 Project ver 5/Assets/Scripts/PlayerExp.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerExp : MonoBehaviour
+{
+    public float curExp;
+    public float maxExp;
+    public int curLevel;
+    public Image ExpImage;
+    public Text LevelText;
+    public GameObject Helper;
+
+    //reference
+    PlayerCombat playerCom;
+    Player playerHea;
+
+    Renderer rend;
+
+    private void Start()
+    {
+        playerCom = FindObjectOfType<PlayerCombat>();
+        playerHea = FindObjectOfType<Player>();
+        rend =GetComponent<Renderer>();
+    }
+
+    private void OnEnable()
+    {
+        curExp = 0;
+        curLevel = 1;
+        Helper.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if(curExp >= maxExp)
+        {
+            curExp -= maxExp;
+            curLevel +=1;
+            StartCoroutine("OnHelper");
+            Bonus();
+            ExpImage.fillAmount = Mathf.Lerp(ExpImage.fillAmount, curExp/maxExp, 10*Time.deltaTime);
+        }
+        else
+        {
+            ExpImage.fillAmount = Mathf.Lerp(ExpImage.fillAmount, curExp/maxExp, 10*Time.deltaTime);
+        }
+
+        LevelText.text = "Level " + curLevel.ToString();
+    }
+    //Generate helper 7 seconds
+    IEnumerator OnHelper()
+    {
+        Helper.SetActive(true);
+        yield return new WaitForSeconds(7f);
+        Helper.SetActive(false);
+    }
+    //Increases Max health by 20. Restore all health losses. Added laser bullets and shotgun bullets
+    void Bonus()
+    {
+        playerHea.MaxHealth += 20;
+        playerHea.Health = playerHea.MaxHealth;
+        playerCom.LaserBullet += 10;
+        playerCom.ShotgunBullet += 12;
+    }
+}
---
+++ 4550 Project ver 5/Assets/Scripts/fireController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fireController : MonoBehaviour
+{
+    public float spd;
+    Rigidbody2D bod;
+    Enemy enemy;
+
+    private void Awake()
+    {
+        bod = GetComponent<Rigidbody2D>();
+        enemy = FindObjectOfType<Enemy>();
+    }
+
+    private void OnEnable()
+    {
+        bod.AddForce(transform.up * spd);
+        Invoke("Disable", 2f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Enemy enemy = collision.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(20);
+            Invoke("Disable", 0.001f);
+        }
+    }
+
+    void Disable()
+    {
+        gameObject.SetActive(false);
+    }
+}
